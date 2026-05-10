70bd50c85c7b6c0e3a94a7b218a33a8bc56bb305
Antonio3732 <ammirati.antonio1@gmail.com>
2026-05-10 22:17:23 +0200

    Aggiustato label

--- database/seeders/PlatformSeeder.php
+++ database/seeders/PlatformSeeder.php
@@ -10,12 +10,12 @@ class PlatformSeeder extends Seeder
     public function run(): void
     {
         $platforms = [
-            ['name' => 'Steam', 'slug' => 'steam', 'color' => '#1b2838'],
+            ['name' => 'Steam', 'slug' => 'steam', 'color' => '#66c0f4'],
             ['name' => 'Epic Games', 'slug' => 'epic-games', 'color' => '#0078f2'],
             ['name' => 'GOG', 'slug' => 'gog', 'color' => '#86328a'],
             ['name' => 'Origin', 'slug' => 'origin', 'color' => '#f56c2d'],
             ['name' => 'Battle.net', 'slug' => 'battle-net', 'color' => '#148eff'],
-            ['name' => 'PlayStation Network', 'slug' => 'psn', 'color' => '#003791'],
+            ['name' => 'PlayStation Network', 'slug' => 'psn', 'color' => '#0070d1'],
             ['name' => 'Xbox', 'slug' => 'xbox', 'color' => '#107c10'],
             ['name' => 'Nintendo eShop', 'slug' => 'nintendo-eshop', 'color' => '#e60012'],
         ];
